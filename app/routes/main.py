e9fc6e7b71630af5e9ab987032fc18b3030962f5
José Fernández <josemfb@gmail.com>
2026-03-19 16:05:52 -0300

    Creates basic routes

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, redirect, url_for
+from app import db
+from app.models import Usuario
 
 main_bp = Blueprint('main', __name__)
 
-# TODO: Dashboard
+@main_bp.route('/')
+def index():
+    return render_template('base.html')
+
+@main_bp.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    ...
