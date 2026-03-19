d0ff8f949eaa5a00a62a3ca2b7d993fb543d16e7
José Fernández <josemfb@gmail.com>
2026-03-19 16:05:38 -0300

    Initialize sqlalchemy extension

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'IsmaelValdésVergara'
     basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'quintanet.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
