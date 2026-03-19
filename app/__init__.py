3072fdf16997221c2954272a78172bcb6e97f8f5
José Fernández <josemfb@gmail.com>
2026-03-19 16:03:07 -0300

    Initialize sqlalchemy extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
+
+# Initialize extensions
+db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # Initialize Flask extensions
+    db.init_app(app)
 
     # Register Blueprints
     from .routes.main import main_bp
